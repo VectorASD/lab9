bf6451e3e7fc1189f2c9d8beb25d854b3875d0ad
VectorASD <60642904+VectorASD@users.noreply.github.com>
2023-04-27 23:45:03 +0700

    Добавил Log и реализовал считывание из SQLite'а

---
+++ AirportTable/Models/BaseReader.cs
@@ -0,0 +1,23 @@
+﻿using AirportTable.Models;
+using AirportTable.ViewModels;
+using Microsoft.Data.Sqlite;
+using System.IO;
+using System.Linq;
+
+namespace AirportTimeTable.Models {
+    public class BaseReader {
+        public BaseReader() {
+            string path = (Directory.GetCurrentDirectory().Contains("AirportTable") ? "../../.." : "AirportTable") + "/Assets/Misc/storage.db";
+            using var con = new SqliteConnection("Data Source=" + path);
+            con.Open();
+            using var reader = new SqliteCommand("SELECT * FROM content", con).ExecuteReader();
+            if (!reader.HasRows) return;
+            while (reader.Read()) {
+                var row = Enumerable.Range(0, reader.VisibleFieldCount).Select(x => reader[x]).ToArray();
+                // Log.Write("YEAH " + Utils.Obj2json(row));
+                long day = (long) row[1];
+                Log.Write("Yeah: " + day + " " + row[2] + " " + Utils.Json2obj((string) row[2]));
+            }
+        }
+    }
+}
--- AirportTable/ViewModels/MainWindowViewModel.cs
+++ AirportTable/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,40 @@
-﻿namespace AirportTable.ViewModels {
+﻿using AirportTimeTable.Models;
+using ReactiveUI;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AirportTable.ViewModels {
+    public class Log {
+        static readonly bool use_file = false;
+
+        static readonly List<string> logs = new();
+        static readonly string path = "../../../Log.txt";
+        static bool first = true;
+
+        public static MainWindowViewModel? Mwvm { private get; set; }
+        public static void Write(string message, bool without_update = false) {
+            if (!without_update) {
+                foreach (var mess in message.Split('\n')) logs.Add(mess);
+                while (logs.Count > 55) logs.RemoveAt(0);
+
+                if (Mwvm != null) Mwvm.Logg = string.Join('\n', logs);
+            }
+
+            if (use_file) {
+                if (first) File.WriteAllText(path, message + "\n");
+                else File.AppendAllText(path, message + "\n");
+                first = false;
+            }
+        }
+    }
+
     public class MainWindowViewModel: ViewModelBase {
-        public string Greeting => "Welcome to Avalonia!";
+        private string log = "";
+        public string Logg { get => log; set { this.RaiseAndSetIfChanged(ref log, value); } }
+
+        public MainWindowViewModel() {
+            Log.Mwvm = this;
+            new BaseReader();
+        }
     }
 }
