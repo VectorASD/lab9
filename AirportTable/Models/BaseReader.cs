913ef96429cbf26bed932d344f7d9be250a44789
VectorASD <60642904+VectorASD@users.noreply.github.com>
2023-04-28 02:02:38 +0700

    Накачал базу данных ЧЕТВЁРТЫМ днём! ;'-}}}}}}} Ну и ещё оптимизровал создание и использование элементов таблицы

--- AirportTable/Models/BaseReader.cs
+++ AirportTable/Models/BaseReader.cs
@@ -8,7 +8,7 @@ using System.Linq;
 
 namespace AirportTimeTable.Models {
     public class BaseReader {
-        public object[][] data = Array.Empty<object[]>();
+        public TableItem[][] data = Array.Empty<TableItem[]>();
         public Dictionary<string, Bitmap> images = new();
 
         public BaseReader() {
@@ -16,28 +16,34 @@ namespace AirportTimeTable.Models {
             using var con = new SqliteConnection("Data Source=" + path);
             con.Open();
 
-            using (var reader = new SqliteCommand("SELECT * FROM content", con).ExecuteReader()) {
-                if (!reader.HasRows) return;
+            using (var reader2 = new SqliteCommand("SELECT * FROM images", con).ExecuteReader()) {
+                if (!reader2.HasRows) return;
 
-                var res = new List<object>[3];
-                for (int i = 0; i < 3; i++) res[i] = new();
-
-                while (reader.Read()) {
-                    var row = Enumerable.Range(0, reader.VisibleFieldCount).Select(x => reader[x]).ToArray();
-                    long day = (long) row[1];
-                    res[day].Add(Utils.Json2obj((string) row[2]) ?? throw new System.Exception("Чё?!"));
+                while (reader2.Read()) {
+                    var row = Enumerable.Range(0, reader2.VisibleFieldCount).Select(x => reader2[x]).ToArray();
+                    var id = (string) row[0];
+                    images[id] = ((string) row[1]).Base64toBitmap();
                 }
-                data = res.Select(x => x.ToArray()).ToArray();
             }
 
-            using var reader2 = new SqliteCommand("SELECT * FROM images", con).ExecuteReader();
-            if (!reader2.HasRows) return;
+            using var reader = new SqliteCommand("SELECT * FROM content", con).ExecuteReader();
+            if (!reader.HasRows) return;
 
-            while (reader2.Read()) {
-                var row = Enumerable.Range(0, reader2.VisibleFieldCount).Select(x => reader2[x]).ToArray();
-                var id = (string) row[0];
-                images[id] = ((string) row[1]).Base64toBitmap();
+            var res = new List<TableItem>[4];
+            for (int i = 0; i < 4; i++) res[i] = new();
+
+            while (reader.Read()) {
+                var row = Enumerable.Range(0, reader.VisibleFieldCount).Select(x => reader[x]).ToArray();
+                long day = (long) row[1];
+                var obj = Utils.Json2obj((string) row[2]) ?? throw new System.Exception("Чё?!");
+                res[day].Add(new TableItem((object[]) obj, this));
             }
+            data = res.Select(x => x.ToArray()).ToArray();
+        }
+
+        // Рандомит некоторые вещи, связанные с текущим временем ;'-}
+        public void TimeBomb() {
+
         }
     }
 }
